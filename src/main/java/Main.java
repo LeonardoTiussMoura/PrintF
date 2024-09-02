c248b0f1ebe451fbc47ade252820abb9233a3e6a
LeonardoTiussMoura <167460740+LeonardoTiussMoura@users.noreply.github.com>
2024-09-02 12:27:39 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    int idade = 15;
+    String nome = "Leonardo";
+    System.out.println("O meu nome é: " + nome + " Minha idade é:+" + idade);
+    System.out.printf("Minha idade é: %d", idade );
+  }
+}
